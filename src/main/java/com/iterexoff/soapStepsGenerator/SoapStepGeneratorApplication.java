6ab58611164349f6fa0a93a18fa36e0b9ed138df
Andrey.Pustogryak <pustogryak.a@gmail.com>
2023-01-12 23:16:05 +0300

    added work with list of wsdl interfaces

--- src/main/java/com/iterexoff/soapStepsGenerator/SoapStepGeneratorApplication.java
+++ src/main/java/com/iterexoff/soapStepsGenerator/SoapStepGeneratorApplication.java
@@ -8,6 +8,8 @@ import com.iterexoff.soapStepsGenerator.utils.ClassUtils;
 import java.lang.reflect.Method;
 import java.net.URLClassLoader;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public class SoapStepGeneratorApplication {
@@ -16,28 +18,33 @@ public class SoapStepGeneratorApplication {
         Path pathWithClasses = Path.of("");
         Optional<URLClassLoader> optionalUrlClassLoader = ClassUtils.getURLClassLoader(pathWithClasses);
 
-        optionalUrlClassLoader.ifPresent(urlClassLoader -> {
-            String classesPathByDot = ClassUtils.getClassesPathByDot(pathWithClasses);
-            Optional<Class<?>> optionalFoundWSDLInterface = ClassUtils.loadClassByName(urlClassLoader, pathWithClasses, classesPathByDot, "");
+        List<String> wsdlInterfaces = Arrays.asList("");
 
+        String portTypeServiceClassName = "";
 
-            optionalFoundWSDLInterface
-                    .ifPresent(wsdlInterface -> {
-                        for (Method declaredMethod : wsdlInterface.getDeclaredMethods()) {
-                            SoapCallGenerateContext generateContext = new SoapCallGenerateContext()
-                                    .setWsdlInterfaceClass(wsdlInterface)
-                                    .setWsdlMethod(declaredMethod)
-                                    .setWsdlServiceClass(ClassUtils.loadClassByName(urlClassLoader, pathWithClasses, classesPathByDot, "").orElse(null))
-                                    .setInitialWsdlInterfaceName("")
-                                    .setExternalAbstractSoapStepPackageName("");
-                            generateContext.setResultStepsPackageName("")
-                                    .setExcludeFromPackageName("")
-                                    .setExternalDateUtilPackageName("")
-                                    .setResultsJavaFilePath(Path.of("target/generated-sources"));
-                            SoapCallGenerator.getInstance().generate(generateContext);
-                            JavaFilesGenerator.getInstance().generate(generateContext);
-                        }
-                    });
+        wsdlInterfaces.forEach(wsdlInterfaceStr -> {
+            optionalUrlClassLoader.ifPresent(urlClassLoader -> {
+                String classesPathByDot = ClassUtils.getClassesPathByDot(pathWithClasses);
+                Optional<Class<?>> optionalFoundWSDLInterface = ClassUtils.loadClassByName(urlClassLoader, pathWithClasses, classesPathByDot, wsdlInterfaceStr);
+
+                optionalFoundWSDLInterface
+                        .ifPresent(wsdlInterface -> {
+                            for (Method declaredMethod : wsdlInterface.getDeclaredMethods()) {
+                                SoapCallGenerateContext generateContext = new SoapCallGenerateContext()
+                                        .setWsdlInterfaceClass(wsdlInterface)
+                                        .setWsdlMethod(declaredMethod)
+                                        .setWsdlServiceClass(ClassUtils.loadClassByName(urlClassLoader, pathWithClasses, classesPathByDot, portTypeServiceClassName).orElse(null))
+                                        .setInitialWsdlInterfaceName(wsdlInterfaceStr)
+                                        .setExternalAbstractSoapStepPackageName("");
+                                generateContext.setResultStepsPackageName("")
+                                        .setExcludeFromPackageName("")
+                                        .setExternalDateUtilPackageName("")
+                                        .setResultsJavaFilePath(Path.of("target/generated-sources"));
+                                SoapCallGenerator.getInstance().generate(generateContext);
+                                JavaFilesGenerator.getInstance().generate(generateContext);
+                            }
+                        });
+            });
         });
     }
 
